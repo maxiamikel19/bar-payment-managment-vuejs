d435aa388e2ce78ad090d3018ac5f502cd2b2848
maxloversist19@gmail.com <maxloversist19@gmail.com>
2024-04-17 11:50:37 -0300

    Setup inicial

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import BillControlMain from "./components/BillControlMain.vue";
+
+
+
+  components: { BillControlMain }</script>
+
+<template>
+  <bill-control-main />
+</template>
+
+<style scoped>
+
+</style>
